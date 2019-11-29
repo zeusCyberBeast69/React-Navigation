b775c30c340ad84c1b84959963af06379ddbfba1
zeusCyberBeast69 <58336047+zeusCyberBeast69@users.noreply.github.com>
2019-11-29 19:54:47 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React,{Component} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import { createStackNavigator } from 'react-navigation-stack';
 class App extends Component {
    
     render(){
